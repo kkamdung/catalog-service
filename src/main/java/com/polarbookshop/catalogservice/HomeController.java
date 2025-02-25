7ed61d613c9b82754621f9ea60fea51dce8d9f1b
kkamdung <kkamdung@empal.com>
2025-02-25 15:53:46 +0900

    스프링 설정 프로세서 의존성 추가 사용자 정의 프로퍼티 정의, 사용자 정의 프로퍼티 클래스 작성 및 코드 반영

--- src/main/java/com/polarbookshop/catalogservice/HomeController.java
+++ src/main/java/com/polarbookshop/catalogservice/HomeController.java
@@ -1,14 +1,19 @@
 package com.polarbookshop.catalogservice;
 
+import com.polarbookshop.catalogservice.config.PolarProperties;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequiredArgsConstructor
 @RestController
 public class HomeController {
 
+    private final PolarProperties polarProperties;
+
     @GetMapping("/")
     public String getGreeting() {
-        return "도서 카탈로그에 오신 것을 환영합니다!";
+        return polarProperties.getGreeting();
     }
 
 }
