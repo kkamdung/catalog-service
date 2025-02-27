76f38929b6dfd33e2f75164ad68bf823342d2e9a
kkamdung <kkamdung@empal.com>
2025-02-27 15:02:39 +0900

    스프링 데이터 JDBC 리포지토리 적용

--- src/main/java/com/polarbookshop/catalogservice/demo/BookDataLoader.java
+++ src/main/java/com/polarbookshop/catalogservice/demo/BookDataLoader.java
@@ -8,6 +8,8 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @RequiredArgsConstructor
 @Component
 @Profile("testdata")
@@ -17,11 +19,12 @@ public class BookDataLoader {
 
     @EventListener(ApplicationReadyEvent.class)
     public void loadBookTestData() {
+        bookRepository.deleteAll();
+
         Book book1 = Book.of("1234567891", "Northern Lights", "Lyra Silverstar", 9.90);
         Book book2 = Book.of("1234567892", "Polar Journey", "Iorek Polarson", 12.90);
 
-        bookRepository.save(book1);
-        bookRepository.save(book2);
+        bookRepository.saveAll(List.of(book1, book2));
     }
 
 }
