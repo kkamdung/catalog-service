545d8cd508eea84a5330b07c3e196918ed5923e9
kkamdung <kkamdung@empal.com>
2025-04-09 16:32:39 +0900

    testdata 프로필 적용 시, isbn 중복으로 인해 오류 발생할 경우 컨테이너 시작이 안되는 문제 수정

--- src/main/java/com/polarbookshop/catalogservice/demo/BookDataLoader.java
+++ src/main/java/com/polarbookshop/catalogservice/demo/BookDataLoader.java
@@ -8,6 +8,8 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
+import java.util.Optional;
+
 @RequiredArgsConstructor
 @Component
 @Profile("testdata")
@@ -20,8 +22,15 @@ public class BookDataLoader {
         Book book1 = Book.of("1234567891", "Northern Lights", "Lyra Silverstar", 9.90, "Polarsophia");
         Book book2 = Book.of("1234567892", "Polar Journey", "Iorek Polarson", 12.90, "Polarsophia");
 
-        bookRepository.save(book1);
-        bookRepository.save(book2);
+        Optional<Book> optBook1 = bookRepository.findByIsbn(book1.isbn());
+        if (optBook1.isEmpty()) {
+            bookRepository.save(book1);
+        }
+
+        Optional<Book> optBook2 = bookRepository.findByIsbn(book2.isbn());
+        if (optBook2.isEmpty()) {
+            bookRepository.save(book2);
+        }
     }
 
 }
