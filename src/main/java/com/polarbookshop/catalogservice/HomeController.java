de0e090002ccd99ed80a362788112747583e85ee
kkamdung <kkamdung@empal.com>
2026-04-26 14:11:48 +0900

    사용자 정의 프로퍼티 설정 클래스 주입 및 동작 확인

--- src/main/java/com/polarbookshop/catalogservice/HomeController.java
+++ src/main/java/com/polarbookshop/catalogservice/HomeController.java
@@ -1,14 +1,19 @@
 package com.polarbookshop.catalogservice;
 
+import com.polarbookshop.catalogservice.config.PolarProperties;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequiredArgsConstructor
 @RestController
 public class HomeController {
 
+    private final PolarProperties polarProperties;
+
     @GetMapping("/")
     public String getGreeting() {
-        return "Welcome to the book catalog!";
+        return polarProperties.getGreeting();
     }
 
 }
