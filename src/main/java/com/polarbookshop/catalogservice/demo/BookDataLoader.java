744ba6ec965cc3d5c9631b853fd240e5bb5df348
kkamdung <kkamdung@empal.com>
2026-06-30 14:54:02 +0900

    스프링 데이터에서 제공하는 메소드를 사용하도록 코드 수정

--- src/main/java/com/polarbookshop/catalogservice/demo/BookDataLoader.java
+++ src/main/java/com/polarbookshop/catalogservice/demo/BookDataLoader.java
@@ -8,6 +8,8 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @RequiredArgsConstructor
 @Component
 @Profile("testdata")
@@ -19,8 +21,7 @@ public class BookDataLoader {
     public void loadBookTestData() {
         var book1 = Book.of("1234567891", "Northern Lights", "Lyra Silverstar", 9.90);
         var book2 = Book.of("1234567892", "Polar Journey", "Iorek Polarson", 12.90);
-        bookRepository.save(book1);
-        bookRepository.save(book2);
+        bookRepository.saveAll(List.of(book1, book2));
     }
 
 }
