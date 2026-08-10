3b1931feaccaa781aeb59bc48c59efd63473ea15
kkamdung <kkamdung@empal.com>
2026-08-10 17:01:04 +0900

    테스트 코드 수정

--- src/test/java/com/polarbookshop/catalogservice/web/BookJsonTests.java
+++ src/test/java/com/polarbookshop/catalogservice/web/BookJsonTests.java
@@ -18,6 +18,8 @@ class BookJsonTests {
     void testSerialize() throws Exception {
         var book = new Book(394L, "1234567890", "Title", "Author", 9.90, 21);
         var jsonContent = json.write(book);
+        assertThat(jsonContent).extractingJsonPathNumberValue("$.id")
+                .isEqualTo(book.id().intValue());
         assertThat(jsonContent).extractingJsonPathStringValue("@.isbn")
                 .isEqualTo(book.isbn());
         assertThat(jsonContent).extractingJsonPathStringValue("@.title")
@@ -26,6 +28,8 @@ class BookJsonTests {
                 .isEqualTo(book.author());
         assertThat(jsonContent).extractingJsonPathNumberValue("@.price")
                 .isEqualTo(book.price());
+        assertThat(jsonContent).extractingJsonPathNumberValue("@.version")
+                .isEqualTo(book.version());
     }
 
     @Test
